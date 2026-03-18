a6e7bf161849c7eceea5f37a1f2dfbddc9171a77
조성현 <sunny1762@naver.com>
2026-03-18 11:27:20 +0900

    B-2문제 풀이

---
+++ test-webdesign/B-1/js/index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    //슬라이드
+    let count = 0
+    const slide = document.querySelector(".train")
+
+    setInterval(()=>{
+        
+        count++
+        slide.style.transform=(`translateX(${-33.33*count}%)`)
+        slide.style.transition=(`all 0.5s ease 0s`)
+        if(count>2){
+            count=0
+            slide.style.transform=(`translateX(0%)`)
+        }
+
+    },1000)
+
+    //팝업
+    const btnClose = document.querySelector(".btn-close")
+    const btnPopup = document.querySelector(".btn-popup")
+    const popup = document.querySelector(".popup")
+
+    btnPopup.addEventListener('click',()=>{
+        popup.classList.add("on")
+    })
+    btnClose.addEventListener('click',()=>{
+        popup.classList.remove("on")
+    })
+
+    //탭메뉴
+    const tabs = document.querySelectorAll(".tabs>span")
+    const tabDesc = document.querySelectorAll(".tab-desc>ul")
+
+    tabs.forEach((tab,i)=>{
+        tab.addEventListener("click",()=>{
+            tabs.forEach((span)=>{span.classList.remove("on")})
+            tabs[i].classList.add("on")
+            tabDesc.forEach((ul)=>{ul.classList.remove("on")})
+            tabDesc[i].classList.add("on")
+        })
+    })
+})
